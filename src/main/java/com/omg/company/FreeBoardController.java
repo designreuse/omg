ae478ec51218be956cf456f70687cbe34d6a45b0
ptsgbny <ptsgbny@nate.com>
2015-01-29 15:24:28 +0900

    정렬

--- src/main/java/com/omg/company/FreeBoardController.java
+++ src/main/java/com/omg/company/FreeBoardController.java
@@ -13,38 +13,39 @@ import com.omg.dto.*;
 import com.omg.service.*;
 
 @Controller
-@RequestMapping(value="/freeboard")
+@RequestMapping(value = "/freeboard")
 public class FreeBoardController {
 	@Autowired
 	private FreeBoardService freeBoardService;
 	@Autowired
 	private CommentsService commentsService;
-	
-	@RequestMapping(value="/freewrite", method=RequestMethod.GET)
-	public String inputform(HttpSession session){
+
+	@RequestMapping(value = "/freewrite", method = RequestMethod.GET)
+	public String inputform(HttpSession session) {
 		return "pages/freeboard/freewrite";
 	}
-	
-	@RequestMapping(value="/freewrite", method=RequestMethod.POST)
-	public String input(HttpSession session, FreeBoard input){
-		Employees emps = (Employees)session.getAttribute("user");
+
+	@RequestMapping(value = "/freewrite", method = RequestMethod.POST)
+	public String input(HttpSession session, FreeBoard input) {
+		Employees emps = (Employees) session.getAttribute("user");
 		input.setWriter(emps.getEmployeeId());
 		freeBoardService.insertList(input);
 		return "redirect:index/1";
 	}
-	
+
 	@RequestMapping("/index")
-	public String index(HttpSession session){
-		Employees user = (Employees)session.getAttribute("user");
-		if(user == null){
+	public String index(HttpSession session) {
+		Employees user = (Employees) session.getAttribute("user");
+		if (user == null) {
 			return "redirect:../../login";
 		}
-		return "redirect:index/1" ;
+		return "redirect:index/1";
 	}
-	
-	//페이지별로 글오는 것.
+
+	// 페이지별로 글오는 것.
 	@RequestMapping("/index/{page}")
-	public String list(HttpSession session, Model model, @PathVariable("page") int page){		
+	public String list(HttpSession session, Model model,
+			@PathVariable("page") int page) {
 		List<FreeBoard> list = freeBoardService.listBoard(page);
 		List<FreeBoard> notices = freeBoardService.selectEmpName(list);
 		int totalCount = freeBoardService.totalCount();
@@ -53,63 +54,80 @@ public class FreeBoardController {
 		model.addAttribute("totalCount", totalCount);
 		return "pages/freeboard/freeboard";
 	}
+
 	@RequestMapping("/selectListBoard/{page}")
-	public String selectListBoard(HttpSession session, Model model, @PathVariable("page") int page, @RequestParam("title") String title){
+	public String selectListBoard(HttpSession session, Model model,
+			@PathVariable("page") int page, @RequestParam("title") String title) {
 		List<FreeBoard> list = freeBoardService.selectListBoard(page, title);
 		List<FreeBoard> notices = freeBoardService.selectEmpName(list);
 		int totalCount = freeBoardService.totalCount();
 		model.addAttribute("freelist", list);
 		model.addAttribute("page", page);
-		model.addAttribute("title",title);
+		model.addAttribute("title", title);
 		model.addAttribute("totalCount", totalCount);
 
-		
 		return "pages/freeboard/freeboard";
 	}
-	@RequestMapping(value="/freedetail", method=RequestMethod.GET)
-	public String detail(HttpSession session, @RequestParam("freeboardNum") int freeboardNum, Model model/*, @PathVariable("page") int page*/){
-		List<Comments> commentsList = commentsService.commentsList(freeboardNum);
+
+	@RequestMapping(value = "/freedetail", method = RequestMethod.GET)
+	public String detail(HttpSession session,
+			@RequestParam("freeboardNum") int freeboardNum, Model model/*
+																		 * , @
+																		 * PathVariable
+																		 * (
+																		 * "page"
+																		 * ) int
+																		 * page
+																		 */) {
+		List<Comments> commentsList = commentsService
+				.commentsList(freeboardNum);
 		List<Comments> comments = commentsService.selectEmpName(commentsList);
 		FreeBoard freeboard = freeBoardService.freeBoard(freeboardNum);
-		if(freeboard!=null){
+		if (freeboard != null) {
 			freeBoardService.count(freeboardNum);
 			model.addAttribute("freedetail", freeboard);
 			model.addAttribute("commentslist", commentsList);
 		}
 		return "pages/freeboard/freedetail";
 	}
-	@RequestMapping(value="/freedelete")
-	public String delete(HttpSession session, @RequestParam("freeboardNum") int freeboardNum){
+
+	@RequestMapping(value = "/freedelete")
+	public String delete(HttpSession session,
+			@RequestParam("freeboardNum") int freeboardNum) {
 		freeBoardService.delete(freeboardNum);
 		return "redirect:index/1";
 	}
-	
-	@RequestMapping(value="/freeupdate", method=RequestMethod.GET)
-	public String updateform(HttpSession session, @RequestParam("freeboardNum") int freeboardNum ,Model model){
+
+	@RequestMapping(value = "/freeupdate", method = RequestMethod.GET)
+	public String updateform(HttpSession session,
+			@RequestParam("freeboardNum") int freeboardNum, Model model) {
 		FreeBoard board = freeBoardService.freeBoard(freeboardNum);
-		model.addAttribute("freeupdate", board);	
+		model.addAttribute("freeupdate", board);
 		return "pages/freeboard/freeupdate";
 	}
-	
-	@RequestMapping(value="/freeupdate", method=RequestMethod.POST)
-	public String update(HttpSession session, FreeBoard Board){
+
+	@RequestMapping(value = "/freeupdate", method = RequestMethod.POST)
+	public String update(HttpSession session, FreeBoard Board) {
 		System.out.println(Board.getFreeboardNum());
 		freeBoardService.update(Board);
 		return "redirect:index/1";
-		
+
 	}
-	
-	@RequestMapping(value="/commentswrite", method=RequestMethod.POST)
-	public String commentInput(HttpSession session, Comments comments){
-		Employees emps = (Employees)session.getAttribute("user");
+
+	@RequestMapping(value = "/commentswrite", method = RequestMethod.POST)
+	public String commentInput(HttpSession session, Comments comments) {
+		Employees emps = (Employees) session.getAttribute("user");
 		comments.setCommentsWriter(emps.getEmployeeId());
 		commentsService.insertComments(comments);
-		return "redirect:freedetail?freeboardNum="+comments.getFreeboardNum();
+		return "redirect:freedetail?freeboardNum=" + comments.getFreeboardNum();
 	}
-	@RequestMapping(value="/commentsdelete")
-	public String commentDelete(HttpSession session, @RequestParam("commentsNum") int commentsNum, @RequestParam("freeboardNum") int freeboardNum){
+
+	@RequestMapping(value = "/commentsdelete")
+	public String commentDelete(HttpSession session,
+			@RequestParam("commentsNum") int commentsNum,
+			@RequestParam("freeboardNum") int freeboardNum) {
 		commentsService.deleteComments(commentsNum);
-		return "redirect:freedetail?freeboardNum="+freeboardNum;
+		return "redirect:freedetail?freeboardNum=" + freeboardNum;
 	}
-	
+
 }
