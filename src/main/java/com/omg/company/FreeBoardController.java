3856c780beee85204ab2a43a9251682a1a84fb4a
ptsgbny <ptsgbny@nate.com>
2015-01-29 15:24:10 +0900

    대현 자유게시판 SYSOUT 나오는것 없엠

--- src/main/java/com/omg/company/FreeBoardController.java
+++ src/main/java/com/omg/company/FreeBoardController.java
@@ -1,23 +1,16 @@
 package com.omg.company;
 
-import java.util.List;
+import java.util.*;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
 
-import com.omg.dto.Comments;
-import com.omg.dto.Employees;
-import com.omg.dto.FreeBoard;
-import com.omg.dto.Notice;
-import com.omg.service.CommentsService;
-import com.omg.service.FreeBoardService;
+import com.omg.dto.*;
+import com.omg.service.*;
 
 @Controller
 @RequestMapping(value="/freeboard")
@@ -29,9 +22,6 @@ public class FreeBoardController {
 	
 	@RequestMapping(value="/freewrite", method=RequestMethod.GET)
 	public String inputform(HttpSession session){
-		
-		//System.out.println("writeGet");
-		
 		return "pages/freeboard/freewrite";
 	}
 	
@@ -39,8 +29,7 @@ public class FreeBoardController {
 	public String input(HttpSession session, FreeBoard input){
 		Employees emps = (Employees)session.getAttribute("user");
 		input.setWriter(emps.getEmployeeId());
-		int ret = freeBoardService.insertList(input);
-		//System.out.println("writePost" + ret);
+		freeBoardService.insertList(input);
 		return "redirect:index/1";
 	}
 	
@@ -62,73 +51,50 @@ public class FreeBoardController {
 		model.addAttribute("freelist", list);
 		model.addAttribute("page", page);
 		model.addAttribute("totalCount", totalCount);
-		//System.out.println(list.get(0).getCount());
-		//System.out.println(totalCount);
-
-		
 		return "pages/freeboard/freeboard";
 	}
 	@RequestMapping("/selectListBoard/{page}")
 	public String selectListBoard(HttpSession session, Model model, @PathVariable("page") int page, @RequestParam("title") String title){
-		System.out.println(page);
-		System.out.println(title);
 		List<FreeBoard> list = freeBoardService.selectListBoard(page, title);
 		List<FreeBoard> notices = freeBoardService.selectEmpName(list);
-		System.out.println(list);
 		int totalCount = freeBoardService.totalCount();
 		model.addAttribute("freelist", list);
 		model.addAttribute("page", page);
 		model.addAttribute("title",title);
 		model.addAttribute("totalCount", totalCount);
-		//System.out.println(list.get(0).getCount());
-		//System.out.println(totalCount);
 
 		
 		return "pages/freeboard/freeboard";
 	}
 	@RequestMapping(value="/freedetail", method=RequestMethod.GET)
 	public String detail(HttpSession session, @RequestParam("freeboardNum") int freeboardNum, Model model/*, @PathVariable("page") int page*/){
-		//List<Comments> commentsList = commentsService.commentsList(page);
-		//List<Comments> commentsName = commentsService.selectEmpName(commentsList);
-		//int totalCount = commentsService.commentsTotal();
 		List<Comments> commentsList = commentsService.commentsList(freeboardNum);
 		List<Comments> comments = commentsService.selectEmpName(commentsList);
-/*for (Comments c : commentsList) {
-	System.out.println(c.getCommentsWriter());
-	System.out.println(c.getCommentsContent());
-}*/
 		FreeBoard freeboard = freeBoardService.freeBoard(freeboardNum);
 		if(freeboard!=null){
-		freeBoardService.count(freeboardNum);
-		model.addAttribute("freedetail", freeboard);
-		model.addAttribute("commentslist", commentsList);
-		//model.addAttribute("page", page);
-		//model.addAttribute("totalCount", totalCount);
+			freeBoardService.count(freeboardNum);
+			model.addAttribute("freedetail", freeboard);
+			model.addAttribute("commentslist", commentsList);
 		}
-		//System.out.println(freeboardNum);
 		return "pages/freeboard/freedetail";
 	}
 	@RequestMapping(value="/freedelete")
 	public String delete(HttpSession session, @RequestParam("freeboardNum") int freeboardNum){
-		//System.out.println(freeboardNum);
-		int ret = freeBoardService.delete(freeboardNum);
+		freeBoardService.delete(freeboardNum);
 		return "redirect:index/1";
 	}
 	
 	@RequestMapping(value="/freeupdate", method=RequestMethod.GET)
 	public String updateform(HttpSession session, @RequestParam("freeboardNum") int freeboardNum ,Model model){
 		FreeBoard board = freeBoardService.freeBoard(freeboardNum);
-		model.addAttribute("freeupdate", board);
-		
+		model.addAttribute("freeupdate", board);	
 		return "pages/freeboard/freeupdate";
 	}
 	
 	@RequestMapping(value="/freeupdate", method=RequestMethod.POST)
 	public String update(HttpSession session, FreeBoard Board){
 		System.out.println(Board.getFreeboardNum());
-//		System.out.println("update"+Board.getTitle());
-		int ret = freeBoardService.update(Board);
-//		System.out.println("updatePost" + ret);
+		freeBoardService.update(Board);
 		return "redirect:index/1";
 		
 	}
@@ -137,16 +103,13 @@ public class FreeBoardController {
 	public String commentInput(HttpSession session, Comments comments){
 		Employees emps = (Employees)session.getAttribute("user");
 		comments.setCommentsWriter(emps.getEmployeeId());
-		int ret = commentsService.insertComments(comments);
-		//System.out.println("writePost" + ret);
+		commentsService.insertComments(comments);
 		return "redirect:freedetail?freeboardNum="+comments.getFreeboardNum();
 	}
 	@RequestMapping(value="/commentsdelete")
 	public String commentDelete(HttpSession session, @RequestParam("commentsNum") int commentsNum, @RequestParam("freeboardNum") int freeboardNum){
-		//System.out.println(commentsNum);
-		int ret = commentsService.deleteComments(commentsNum);
+		commentsService.deleteComments(commentsNum);
 		return "redirect:freedetail?freeboardNum="+freeboardNum;
-		//return "redirect:freedetail?freeboardNum="+comments.getFreeboardNum();
 	}
 	
 }
